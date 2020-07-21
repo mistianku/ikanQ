aa7b42aed3d454f9db7f0bf9a57b6b563c8e1ac0
mistian <muh.istian@gmail.com>
2020-07-21 15:11:52 +0700

    comiit awal

---
+++ BackupDatabase/backupdatabase.bat
@@ -0,0 +1,1 @@
+C:\xampp\mysql\bin\mysqldump -P 3306 -h localhost -usa -pspvsql --routines ikan > "E:\BackupDataIstian\Data Istian\Sayuran_multi_company\versi on line\BackupDatabase_ikan_Temp\"ikan.SQL
